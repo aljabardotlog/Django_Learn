aa35eefed18951ce2e045e7d86eb6a2e1b8ab118
Andi Aljabar <andi.aljabar30@gmail.com>
2019-08-17 07:17:28 +0700

    4 commit

--- posts/views.py
+++ posts/views.py
@@ -4,16 +4,37 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    data = {
+        "title": "Create"
+    }
+    return render(request, "index.html", data)
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    data = {
+        "title": "Detail"
+    }
+    return render(request, "index.html", data)
 
 def post_list(request):
-    return render(request, "index.html", {})
+    if request.user.is_authenticated:
+        data = {
+            "title": "List and user is logged in"
+        }
+    else:
+        data = {
+            "title": "List and user is logged out"
+        }
+
+    return render(request, "index.html", data)
 
 def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+    data = {
+        "title": "Update"
+    }
+    return render(request, "index.html", data)
 
 def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+    data = {
+        "title": "Delete"
+    }
+    return render(request, "index.html", data)
